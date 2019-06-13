807e844f689569cc4b43d0d72ce9220a19dcc7a8
francozili22 <francozili22@gmail.com>
2019-06-13 16:40:16 -0400

    added a model to my models file

--- Eventify_app/models.py
+++ Eventify_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(default = '', max_length = 100)
+    lastname = models.CharField(default = '', max_length = 100)
+    picture = models.CharField(default = '', max_length = 100)
+    phonenumber = models.IntegerField()
+    email = models.EmailField(default = '', max_length = 100, unique=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    
+
+    def __str__(self):
+        return self.firstname
+
+
+
