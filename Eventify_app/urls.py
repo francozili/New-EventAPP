deabbc1131dc4b51741d14319f84f28cc98156b4
francozili22 <francozili22@gmail.com>
2019-06-14 12:09:10 -0400

    made a url.py and added routes for the app

--- Eventify_app/urls.py
+++ Eventify_app/urls.py
@@ -1,17 +1,14 @@
 # tunr_app/urls.py
 from django.urls import path, include
 from . import views
-
-urlpatterns = [
-]
-from .views import ItemViewSet, PantryViewSet, UserProfileViewSet
+from .views import EventViewSet, LocationViewSet, UserViewSet
 from rest_framework import routers
 
 
 router = routers.SimpleRouter()
 
-router.register('items', ItemViewSet)
-router.register('pantries', PantryViewSet)
-router.register('userprofiles', UserProfileViewSet)
+router.register('events', EventViewSet)
+router.register('locations', LocationViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = router.urls
