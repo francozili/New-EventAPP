6c72f84f6bf63906ac1f9071928ae3726cb0de8e
francozili22 <francozili22@gmail.com>
2019-06-14 11:24:08 -0400

    made changes to my serializer file added the location serializer

--- Eventify_app/serializers.py
+++ Eventify_app/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
-from Eventify_app.models import User
+from Eventify_app.models import User, Location
 
 # User Serializer
+
+class LocationSerializer(serializers.ModelSerializer):
+    events = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True
+        
+        
+    )
+    user = serializers.PrimaryKeyRelatedField(
+        queryset = UserProfile.objects.all()
+    ) 
+    
+    class Meta:
+        model = Location
+        fields=('id', '__all__', 'events', 'user')
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
